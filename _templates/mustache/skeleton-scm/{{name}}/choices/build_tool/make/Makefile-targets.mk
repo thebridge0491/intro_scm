1ac28789e213a5aae2b387f10c237e54cae73873
thebridge0491 <thebridge0491-codelab@yahoo.com>
2022-02-02 18:05:54 +0000

    Update template - update build tools ; fix FFI shared library loading

--- _templates/mustache/skeleton-scm/{{name}}/choices/build_tool/make/Makefile-targets.mk
+++ _templates/mustache/skeleton-scm/{{name}}/choices/build_tool/make/Makefile-targets.mk
@@ -4,19 +4,15 @@
 # $* - basename (cur target)  $^ - name(s) (all depns)  $< - name (1st depn)
 # $@ - name (cur target)      $% - archive member name  $? - changed depns
 
-FMTS ?= tar.gz
+FMTS ?= tar.gz,zip
 distdir = $(proj)-$(version)
 
-build/lib/%.so : 
-	-$(LINK.c) -fPIC -shared -Wl,-soname,$*.so.$(ver_major) $^ -o $@.$(version) $(LDLIBS)
-	-cd build/lib ; ln -sf $*.so.$(version) $*.so.$(ver_major) \
-		&& ln -sf $*.so.$(version) $*.so
-build/lib/%.dylib : 
-	-$(LINK.c) -fPIC -dynamiclib -undefined suppress -flat_namespace -Wl,-install_name,@rpath/$*.$(ver_major).dylib,-current_version,$(version),-compatibility_version,$(version) $^ -o lib/$*.$(version).dylib $(LDLIBS)
-	-cd build/lib ; ln -sf $*.$(version).dylib $*.$(ver_major).dylib \
-		&& ln -sf $*.$(version).dylib $*.dylib
+build/$(distdir) : 
+	-@mkdir -p build/$(distdir) ; cp -f exclude.lst build/
+#	#-zip -9 -q --exclude @exclude.lst -r - . | unzip -od build/$(distdir) -
+	-tar --format=posix --dereference --exclude-from=exclude.lst -cf - . | tar -xpf - -C build/$(distdir)
 
-.PHONY: help clean check uninstall install dist doc
+.PHONY: help clean test uninstall install dist doc
 help: ## help
 	@echo "##### subproject: $(proj) #####"
 	@echo "Usage: $(MAKE) [SCHEME="$(SCHEME)"] [target] -- some valid targets:"
@@ -29,25 +25,27 @@ help: ## help
 	done
 clean: ## clean build artifacts
 	-rm -rf build/* build/.??*
-check: tests/ts_main.scm ## run test [TOPTS=""]
+test: tests/ts_main.scm ## run test [TOPTS=""]
 #	export [DY]LD_LIBRARY_PATH=. # ([da|ba|z]sh Linux)
 #	setenv [DY]LD_LIBRARY_PATH . # (tcsh FreeBSD)
-	-cd build ; LD_LIBRARY_PATH=$(LD_LIBRARY_PATH):lib $(SCHEME) -A$(sitelibdir) -A.. -A../tests ../tests/ts_main.scm $(TOPTS)
+	-cd build ; LD_LIBRARY_PATH=$(LD_LIBRARY_PATH):lib $(SCHEME) -A$(sitelibdir) -A. -A.. -A../tests ../tests/ts_main.scm $(TOPTS)
 uninstall install: ## [un]install artifacts
 	-@if [ "uninstall" = "$@" ] ; then \
-		rm -ir $(sitelibdir)/*$(proj)* ; \
-	else mkdir -p $(sitelibdir) ; (cp -fR $(parent) $(sitelibdir)/) ; fi
-dist: ## [FMTS="tar.gz"] archive source code
-	-@mkdir -p build/$(distdir) ; cp -f exclude.lst build/
-#	#-zip -9 -q --exclude @exclude.lst -r - . | unzip -od build/$(distdir) -
-	-tar --format=posix --dereference --exclude-from=exclude.lst -cf - . | tar -xpf - -C build/$(distdir)
-	
+		rm -ir $(sitelibdir)/*$(parent)* ; \
+	else \
+		mkdir -p $(sitelibdir) ; (cp -fR $(parent) $(sitelibdir)/) ; \
+		ls $(sitelibdir) | grep $(parent) ; sleep 3 ; \
+	fi
+dist: | build/$(distdir) ## [FMTS="tar.gz,zip"] archive source code
 	-@for fmt in `echo $(FMTS) | tr ',' ' '` ; do \
 		case $$fmt in \
+			7z) echo "### build/$(distdir).7z ###" ; \
+				rm -f build/$(distdir).7z ; \
+				(cd build ; 7za a -t7z -mx=9 $(distdir).7z $(distdir)) ;; \
 			zip) echo "### build/$(distdir).zip ###" ; \
 				rm -f build/$(distdir).zip ; \
 				(cd build ; zip -9 -q -r $(distdir).zip $(distdir)) ;; \
-			*) tarext=`echo $$fmt | grep -e '^tar$$' -e '^tar.xz$$' -e '^tar.bz2$$' || echo tar.gz` ; \
+			*) tarext=`echo $$fmt | grep -e '^tar$$' -e '^tar.xz$$' -e '^tar.zst$$' -e '^tar.bz2$$' || echo tar.gz` ; \
 				echo "### build/$(distdir).$$tarext ###" ; \
 				rm -f build/$(distdir).$$tarext ; \
 				(cd build ; tar --posix -L -caf $(distdir).$$tarext $(distdir)) ;; \
--- _templates/mustache/skeleton-scm/{{name}}/choices/build_tool/rake/rakefile-targets.rb
+++ _templates/mustache/skeleton-scm/{{name}}/choices/build_tool/rake/rakefile-targets.rb
@@ -16,57 +16,61 @@ desc 'Run tests: rake test\[topt1,topt2\]'
 task :test, [:topt1] => 'tests/ts_main.scm' do |t, topts|
 #	export [DY]LD_LIBRARY_PATH=. # ([da|ba|z]sh Linux)
 #	setenv [DY]LD_LIBRARY_PATH . # (tcsh FreeBSD)
-  sh "cd build ; LD_LIBRARY_PATH=#{ENV['LD_LIBRARY_PATH']}:lib ${SCHEME} -A${sitelibdir} -A.. -A../tests ../#{t.source} #{topts[:topt1]} #{topts.extras.join(' ')} || true"
+  sh "cd build ; env LD_LIBRARY_PATH=#{ENV['LD_LIBRARY_PATH']}:lib #{SCHEME} -A#{SITELIBDIR} -A. -A.. -A../tests ../#{t.source} #{topts[:topt1]} #{topts.extras.join(' ')} || true"
 end
 
 #----------------------------------------
 desc 'Uninstall artifacts'
 task :uninstall do
-  installfiles = FileList["#{sitelibdir}/*#{VARS.proj}*"]  
+  installfiles = FileList["#{SITELIBDIR}/*#{VARS.parent}*"]  
   rm_rf(installfiles) || true
 end
 
 desc 'Install artifacts'
 task :install do
-  mkdir_p("#{sitelibdir}")
-  filelst = FileList["${VARS.parent}"]
-  cp_r(filelst, "#{sitelibdir}") || true
+  mkdir_p("#{SITELIBDIR}")
+  filelst = FileList["#{VARS.parent}"]
+  cp_r(filelst, "#{SITELIBDIR}") || true
+  sh "ls #{SITELIBDIR} | grep #{VARS.parent} ; sleep 3"
 end
 
-file "build/#{VARS.name}-#{VARS.version}" do |p|
+file "build/#{VARS.proj}-#{VARS.version}" do |p|
   mkdir_p(p.name)
   # sh "zip -9 -q -x @exclude.lst -r - . | unzip -od #{p.name} -"
   sh "tar --posix -L -X exclude.lst -cf - . | tar -xpf - -C #{p.name}"
 end
 if defined? Rake::PackageTask
-  Rake::PackageTask.new(VARS.name, VARS.version) do |p|
-    # task("build/#{VARS.name}-#{VARS.version}").invoke
+  Rake::PackageTask.new(VARS.proj, VARS.version) do |p|
+    # task("build/#{VARS.proj}-#{VARS.version}").invoke
     
-    ENV.fetch('FMTS', 'tar.gz').split(',').each{|fmt|
+    ENV.fetch('FMTS', 'tar.gz,zip').split(',').each{|fmt|
       if p.respond_to? "need_#{fmt.tr('.', '_')}="
         p.send("need_#{fmt.tr('.', '_')}=", true)
       else
         p.need_tar_gz = true
       end
     }
-    task(:package).add_description "[FMTS=#{ENV.fetch('FMTS', 'tar.gz')}]"
-    task(:repackage).add_description "[FMTS=#{ENV.fetch('FMTS', 'tar.gz')}]"
+    task(:package).add_description "[FMTS=#{ENV.fetch('FMTS', 'tar.gz,zip')}]"
+    task(:repackage).add_description "[FMTS=#{ENV.fetch('FMTS', 'tar.gz,zip')}]"
   end
 else
-  desc "[FMTS=#{ENV.fetch('FMTS', 'tar.gz')}] Package project distribution"
-  task :dist => ["build/#{VARS.name}-#{VARS.version}"] do |t|
-    distdir = "#{VARS.name}-#{VARS.version}"
+  desc "[FMTS=#{ENV.fetch('FMTS', 'tar.gz,zip')}] Package project distribution"
+  task :package => ["#{VARS.proj}-#{VARS.version}"] do |t|
+    distdir = "#{VARS.proj}-#{VARS.version}"
     
-    ENV.fetch('FMTS', 'tar.gz').split(',').each{|fmt|
+    ENV.fetch('FMTS', 'tar.gz,zip').split(',').each{|fmt|
       case fmt
+      when '7z'
+        rm_rf("#{distdir}.7z") || true
+        sh "7za a -t7z -mx=9 #{distdir}.7z #{distdir}" || true
       when 'zip'
-        rm_rf("build/#{distdir}.zip") || true
-        cd('build') {sh "zip -9 -q -r #{distdir}.zip #{distdir}" || true}
+        rm_rf("#{distdir}.zip") || true
+        sh "zip -9 -q -r #{distdir}.zip #{distdir}" || true
       else
-        # tarext = `echo #{fmt} | grep -e '^tar$' -e '^tar.xz$' -e '^tar.bz2$' || echo tar.gz`.chomp
-        tarext = fmt.match(%r{(^tar$|^tar.xz$|^tar.bz2$)}) ? fmt : 'tar.gz'
-        rm_rf("build/#{distdir}.#{tarext}") || true
-        cd('build') {sh "tar --posix -L -caf #{distdir}.#{tarext} #{distdir}" || true}
+        # tarext = `echo #{fmt} | grep -e '^tar$' -e '^tar.xz$' -e '^tar.zst$' -e '^tar.bz2$' || echo tar.gz`.chomp
+        tarext = fmt.match(%r{(^tar$|^tar.xz$|^tar.zst$|^tar.bz2$)}) ? fmt : 'tar.gz'
+        rm_rf("#{distdir}.#{tarext}") || true
+        sh "tar --posix -L -caf #{distdir}.#{tarext} #{distdir}" || true
       end
     }
   end
